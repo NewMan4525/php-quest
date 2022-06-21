150f8477b28a26dd778844266fc681fc9d1a9a21
Andrey Losev <newman4525@gmail.com>
2022-06-21 16:33:16 +0300

    less2_complite

--- index.php
+++ index.php
@@ -1,6 +1,20 @@
 <?php
-$bool=true;
-$null=null;
 
-var_dump(isset($bool));
-var_dump(isset($null));
+$arrayName2=['wallet'=>500,'pocket'=>'diplom','phone'=>false,
+'list'=>['fruit','bread']
+];
+
+var_dump($arrayName2);
+
+
+$obj=new stdClass;
+$obj->a ='i am property A';
+
+
+
+var_dump($obj);
+
+
+
+
+?>
